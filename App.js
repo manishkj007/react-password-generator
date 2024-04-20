4186af117b1a500e65578c5e3959ee5ccd12d262
manishkj007 <manishkj007@gmail.com>
2024-04-19 22:41:30 -0400

    created all the export components

--- App.js
+++ App.js
@@ -4,3 +4,84 @@ import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from '
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import { COPY_Fail, COPY_SUCCESS } from './message';
+
+const App = () => {
+  const [password, setPassword] = useState("")
+  const [passwordLength, setPasswordLength] = useState(26)
+  const [includeUpperCase, setIncludeUpperCase] = useState(false)
+  const [includeLowerCase, setIncludeLowerCase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+  const handleGeneratePassword = () => {
+    if (!includeUpperCase && !includeLowerCase && !includeNumbers && !includeSymbols) {
+      notify("To generate password you must select atleast one checkbox", true)
+    }
+    else {
+      let characterList = ""
+      if (includeNumbers) {
+        characterList = characterList + numbers
+      }
+      if (includeUpperCase) {
+        characterList = characterList + upperCaseLetters
+      }
+      if (includeLowerCase) {
+        characterList = characterList + lowerCaseLetters
+      }
+      if (includeSymbols) {
+        characterList = characterList + specialCharacters
+      }
+      setPassword(createPassword(characterList))
+      notify("Password is generated successfully", false)
+    }
+
+
+  }
+  const createPassword = (characterList) => {
+    let password = ""
+    const characterListLength = characterList.length
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength)
+      password = password + characterList.charAt(characterIndex)
+    }
+    return password
+  }
+  const copyToClipboard = (password) => {
+
+    navigator.clipboard.writeText(password)
+  }
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      toast.error(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    else {
+      toast(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+
+  }
+  const handleCopyPassword = (e) => {
+    if (password === "") {
+      notify(COPY_Fail, true)
+    }
+    else {
+      copyToClipboard(password)
+      notify(COPY_SUCCESS)
+    }
+
+  }
+
