eee5a410f364b4276b6bd9050e563cc0b57620ff
Dhaiwat Maheria <67494487+dhaiwat15@users.noreply.github.com>
2024-04-19 23:05:54 -0400

    Update Character.js

--- Character.js
+++ Character.js
@@ -2,3 +2,5 @@ export const numbers = '0123456789'
 export const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 export const lowerCaseLetters ='abcdefghijklmnopqrstuvwxyz'
 export const specialCharacters = "!'^+%&/()=?_#$½§{[]}|;:>÷`<.*-@é"
+
+//Character slecting file
